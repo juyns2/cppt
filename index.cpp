4b9ab37b57504de6fec5a84b00bb4b7a19abfcc4
Nguyễn Duy Tân <62575289+juyns2@users.noreply.github.com>
2020-12-11 18:46:28 +0700

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,8 +1,10 @@
-#include <"iostream">
-using namespace std;
-
-void main()
-{ 
-  cout<<"hello";
-}
-
+<!DOCTYPE html>
+<html>
+<head>
+<title>Day la tieu de</title>
+</head>
+<body>
+<h1>Day la dau de</h1>
+<p>Phan noi dung cua tai lieu ...</p>
+</body>
+</html>
